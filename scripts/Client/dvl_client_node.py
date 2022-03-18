2f0351aa73cd38fa7653afd6ae38db4ecbdfc0fb
Hector <hector.montes1@upr.edu>
2022-03-18 07:52:19 -0400

    Added documentation, wip

--- scripts/Client/dvl_client_node.py
+++ scripts/Client/dvl_client_node.py
@@ -2,30 +2,42 @@
 
 from time import sleep
 from std_msgs.msg import Header
-# from dvl_pub_pkg.msg import Wayfinder_DVL
 from dvl.msg import Raw_DVL
 from rospy import Publisher, Rate, init_node, is_shutdown, Time
 from requests import get
 from requests.exceptions import ConnectionError
 
-DEFAULT_WAIT_TIME_FOR_RETRY = 3
-
-
 class DVLNodePublisher:
 
-    URL_ROUTE = 'http://127.0.0.1:5000/dvl'
+    URL_ROUTE = 'http://127.0.0.1:5000/dvl' # Default url route to get dvl information, should never change
+
+    """Used when the node is running, if the http server is not running after max requests amount to server is reached it will wait by this value to try again."""
     DEFAULT_SLEEP_TIMEOUT = 3
 
     def __init__(self, topic_to_publish_to, node_base_name, rate=10, default_queue_size=10):
-        self.dvl_msg_data = Raw_DVL()        
-        self.pub = Publisher(topic_to_publish_to, Raw_DVL, queue_size=default_queue_size)
-        init_node(node_base_name, anonymous=True)
-        self.rate = Rate(rate)
+        """
+        Creates DVLNodePublisher instance
+
+        Args:
+            topic_to_publish: str -- Topic name the ros publisher will be publishing to.
+            node_base_name: str -- Name of the ros node.
+            rate: int (default 10) -- Rate at which the ros node will operate on in amount of Hz.
+            default_queue_size: int (default 10) -- Limits the amount of messages in the queue if a subscriber is not fast enough to receive them.
+
+        Returns:
+            Instance of DVLNodePublisher
+
+        """
+        self.dvl_msg_data = Raw_DVL() # Create message instance, instance will be reused by node to provide information
+        self.pub = Publisher(topic_to_publish_to, Raw_DVL, queue_size=default_queue_size) # Create ros publisher to given arguments
+        init_node(node_base_name, anonymous=True) # Init ros node
+        self.rate = Rate(rate) # Set rate for ros node
 
     def publish_dvl_message(self):
+        """Requests data from http server and publishes the message to ros, if the request fails it waits for the amount specified rate."""
         if self._request_dvl_data():            
-            self.dvl_msg_data.header = self._get_msg_header()
-            self.pub.publish(self.dvl_msg_data)        
+            self.dvl_msg_data.header = self._get_msg_header() # Sets messageheader to message
+            self.pub.publish(self.dvl_msg_data) # Message is published to ros with publisher
         self.rate.sleep()
 
     def _request_dvl_data(self):
@@ -48,6 +60,15 @@ class DVLNodePublisher:
                     setattr(self.dvl_msg_data, key, val)
 
     def _get_msg_header(self):
+        """
+        Creates a header instance to be used for the message that is going to be published.
+
+        Returns:
+            msg_header -- ros Header message instance
+
+        Raises:
+            ROSException: ros must be initialized first, usually with init method from rospy
+        """
         msg_header = Header()
         msg_header.stamp = Time.now()
         return msg_header
@@ -55,14 +76,14 @@ class DVLNodePublisher:
             
 
 
-if __name__=='__main__':
-    dvl_pub = DVLNodePublisher('dvl', 'Wayfinder_DVL_Filter')
-    while not is_shutdown():
+if __name__=='__main__': # Code runs if this script is loaded as main
+    dvl_pub = DVLNodePublisher('dvl', 'Wayfinder_DVL_Filter') # Creates instance of DVLNodePublisher
+    while not is_shutdown(): # Keep working while ROS on
         try:
-            dvl_pub.publish_dvl_message()
-        except ConnectionError:
-            print 'Could not reach http server, retrying in %ss...' % DEFAULT_WAIT_TIME_FOR_RETRY
-            sleep(DEFAULT_WAIT_TIME_FOR_RETRY)
+            dvl_pub.publish_dvl_message() # Publish dvl message to ros
+        except ConnectionError: # If the server is not running or having errors this exception is raised and node will wait until it tries again
+            print 'Could not reach http server, retrying in %ss...' % DVLNodePublisher.DEFAULT_SLEEP_TIMEOUT
+            sleep(DVLNodePublisher.DEFAULT_SLEEP_TIMEOUT)
         
 
 
--- src/lib/Server/server.py
+++ src/lib/Server/server.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from os import system
 from sys import platform
 
+# Used for text color in terminal
 ANSI_YELLOW = '\033[93m'
 ANSI_ESC = '\033[0m'
 
@@ -25,14 +26,14 @@ PUT = 'PUT'
 OK = 200
 FORBIDDEN = 403
 
-# DVL device object creation
+# DVL device object creation, init both devices for ease of use
 DUMMY_DVL_DATA = DVL_DATA()
-DUMMY_DVL_DEVICE = DVLDummyDevice()
+DUMMY_DVL_DEVICE = DVLDummyDevice() 
 WAYFINDER_DVL = WayfinderDVL()
 
 
 DVL_DEVICE = WAYFINDER_DVL
-DVL_DEVICE = DUMMY_DVL_DEVICE
+DVL_DEVICE = DUMMY_DVL_DEVICE # Always set DVL_DEVICE as dummy, comment this line to use actual wayfinder dvl
 if not DVL_DEVICE.connect():
     print('Connection to DVL failed, program will exit...')
     exit()
@@ -41,12 +42,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def default():
-    '''Default route, returns dummy text.'''
+    '''Default route, returns dummy text. Not used'''
     return 'Dummy Text'
 
 @app.route('/dvl', methods=(GET,))
 def get_dvl_info():
-    '''Method called for route /dvl
+    '''
+    Method called for route /dvl
+    
     RETURN: HTTP response with dvl device data in json format
     '''
     if request.method == GET:
