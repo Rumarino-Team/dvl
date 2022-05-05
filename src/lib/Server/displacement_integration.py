1e161ce4c502b84882669e6840e4dcdb55e2a7e0
Ale <ale.pagan@upr.edu>
2022-05-05 12:03:36 -0400

    Corrected the documentation

--- src/lib/Server/displacement_integration.py
+++ src/lib/Server/displacement_integration.py
@@ -22,7 +22,7 @@ iVelocity: List that represents the x,y and z Initial Velocity
 
 fVelocity: List that represents x,y and z finals velocities
 
-time: List has two elements, the first element is the initial time and the second element is the final velocity.
+time: List that contains two elements, the first element is the initial time and the second element is the final time.
 
 Author:
 
@@ -34,36 +34,30 @@ Ale Pagan Andujar
         
 
 def displacement(iVelocity, fVelocity, time):
-    """This function expects three lists as parameters, the first parameter must be the
-    initial velocity vector, second parameter must be the final velocity vector. The time
-    parameter must have: time[0] = initial time and time[1] = final time. This function
-    will return a list that represents the displacement in x,y and z.
+    """The three arguments for the function must be a List, the first argument must be the
+    initial velocity vector in meters per seconds (m/s), second list must be the final velocity 
+    vector in meters per seconds (m/s). The time parameter must have: time[0] = initial time and
+    time[1] = final time i in seconds. This function will return a list that represents the
+     displacement in x, y and z in meters (m).
     """
 
-    change_velx = fVelocity[0] - iVelocity[0]
-    """
-    This variable will hold the rate of change of velocity in the x direction.
-    """
+    change_velx = fVelocity[0] - iVelocity[0] #This variable will hold the rate of change of velocity in the x direction.
 
-    change_vely = fVelocity[1] - iVelocity[1]
-    """
-    This variable will hold the rate of change in velocity in the y direction.
-    """
 
-    change_velz = fVelocity[2] - iVelocity[2]
-    """
-    This variable will hold the rate of change in velocity in the z direction.
-    """
+    change_vely = fVelocity[1] - iVelocity[1] #This variable will hold the rate of change in velocity in the y direction.
+    
 
-    change_time = time[1] - time[0]
-    """
-    This variable will hold the rate of change in time from the given velocity.
-    """
+    change_velz = fVelocity[2] - iVelocity[2] #This variable will hold the rate of change in velocity in the z direction.
 
-    displacement = [0] * 3
-    """
-    This list will store the displacement in x,y,z respectively.
-    """
+
+    change_time = time[1] - time[0] #This variable will hold the rate of change in time from the given velocity.
+
+
+ 
+
+    displacement = [0] * 3 #This list will store the displacement in x,y,z respectively.
+
+
 
     displacement[0] = change_velx * change_time
     displacement[1] = change_vely * change_time
